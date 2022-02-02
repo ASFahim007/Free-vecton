0846d81b1aaf3c1bf680f80b0b35b39016b21d4a
ASFahim007 <asfahim4321@gmail.com>
2022-02-02 14:37:47 +0600

    popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    $("select").niceSelect();
+    $(".nice").niceSelect();
+
+    // ddslick 
+    $('.options').ddslick(); 
+    
 
 
 
@@ -12,6 +16,7 @@ $(document).ready(function () {
         animateOut: 'fadeOut',
         items: 1,
         autoplayTimeout: 8000,
+        mouseDrag: false,
         responsive:{
             0:{
                 items:1
@@ -26,6 +31,53 @@ $(document).ready(function () {
     })
     
 });
+
+
+
+
+// popup 
+$(".popup-with-zoom-anim").magnificPopup({
+  type: "inline",
+
+  fixedContentPos: false,
+  fixedBgPos: true,
+
+  overflowY: "auto",
+
+  closeBtnInside: false,
+  preloader: false,
+
+  midClick: true,
+  removalDelay: 300,
+  mainClass: "my-mfp-zoom-in",
+
+  gallery: {
+      enabled: true,
+  },
+});
+
+$('.popup-with-move-anim').magnificPopup({
+  type: 'inline',
+
+  fixedContentPos: false,
+  fixedBgPos: true,
+
+  overflowY: 'auto',
+
+  closeBtnInside: true,
+  preloader: false,
+  
+  midClick: true,
+  removalDelay: 300,
+  mainClass: 'my-mfp-slide-bottom'
+});
+
+
+
+
+
+
+
 // sticky searc bar
 const header = document.querySelector(".search_container");
 const sectionOne = document.querySelector(".banner");
@@ -51,23 +103,23 @@ sectionOneObserver.observe(sectionOne);
 
 
 
-//  new account 
-document.querySelector(".sign_up").addEventListener("click", function(){
-    document.querySelector(".create_account").classList.add("active");
-} );
+// //  new account 
+// document.querySelector(".sign_up").addEventListener("click", function(){
+//     document.querySelector(".create_account").classList.add("active");
+// } );
 
-document.querySelector(".create_account .close_btn").addEventListener("click",function(){
-    document.querySelector(".create_account").classList.remove("active");
-} );
+// document.querySelector(".create_account .close_btn").addEventListener("click",function(){
+//     document.querySelector(".create_account").classList.remove("active");
+// } );
 
-// log in 
-document.querySelector(".log_in").addEventListener("click", function(){
-    document.querySelector(".login_form").classList.add("log_active");
-} );
+// // log in 
+// document.querySelector(".log_in").addEventListener("click", function(){
+//     document.querySelector(".login_form").classList.add("log_active");
+// } );
 
-document.querySelector(".login_form .close_log_btn").addEventListener("click",function(){
-    document.querySelector(".login_form").classList.remove("log_active");
-} );
+// document.querySelector(".login_form .close_log_btn").addEventListener("click",function(){
+//     document.querySelector(".login_form").classList.remove("log_active");
+// } );
 
 
 
